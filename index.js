965d353812008a9c09b256514eff1329f7a7b2a7
Marc Jerome Doria <marcj.doria@gmail.com>
2024-10-28 15:13:25 +0800

    Connect to MongoDB

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.send("BACKEND IS CONNECTED!");
@@ -15,3 +15,8 @@ const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("CONNECTED TO MONGODB"))
+  .catch((err) => console.error("Failed to connect to MongoDB:", err));
