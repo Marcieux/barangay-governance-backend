3963d3122f9fc2b9dde3d1146b97a01ddb9f7fd1
Marc Jerome Doria <marcj.doria@gmail.com>
2024-11-15 12:24:25 +0800

    added new arrays in the barangay collection/model
    -new arrays for cafgu, purok chair and tagapamayapa

--- models/barangays-model.js
+++ models/barangays-model.js
@@ -4,7 +4,10 @@ const barangaySchema = mongoose.Schema({
   barangay_name: { type: String, required: true },
   king_id: { type: mongoose.Schema.Types.ObjectId, ref: "People", default: null },
   king_name: { type: String, default: null },
-  prince: [{ type: mongoose.Schema.Types.ObjectId, ref: "Prince", required: true }]
+  prince: [{ type: mongoose.Schema.Types.ObjectId, ref: "Prince", required: true }],
+  cafgu: [{ type: mongoose.Schema.Types.ObjectId, ref: "People", required: true }],
+  purok_chair: [{ type: mongoose.Schema.Types.ObjectId, ref: "People", required: true }],
+  tagapamayapa: [{ type: mongoose.Schema.Types.ObjectId, ref: "People", required: true }]
 });
 
 export const Barangay = mongoose.model("Barangay", barangaySchema, "barangay");
