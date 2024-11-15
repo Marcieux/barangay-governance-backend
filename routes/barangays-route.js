c6e34e9ca1b446d24499ee193f9a93d55c2ae9fb
Marc Jerome Doria <marcj.doria@gmail.com>
2024-11-15 12:25:54 +0800

    update barangay route for adding cafgu, purok chair and tagapamayapa in barangay

--- routes/barangays-route.js
+++ routes/barangays-route.js
@@ -72,4 +72,106 @@ router.put("/:id/prince", async (req, res) => {
   }
 });
 
+router.put("/:id/cafgu", async (req, res) => {
+  const { id } = req.params;
+  const { cafgu } = req.body;
+
+  if (!Array.isArray(cafgu) || cafgu.length === 0) {
+    return res
+      .status(400)
+      .json({ message: "Cafgu array is required and cannot be empty." });
+  }
+
+  try {
+    const updatedBarangay = await Barangay.findByIdAndUpdate(
+      id,
+      { $addToSet: { cafgu: { $each: cafgu } } },
+      { new: true }
+    );
+
+    if (!updatedBarangay) {
+      return res.status(404).json({ message: "Barangay not found" });
+    }
+
+    res.json({
+      message: "Cafgu added to barangay successfully",
+      barangay: updatedBarangay,
+    });
+  } catch (err) {
+    console.error("Error updating barangay with cafgu:", err);
+    res.status(500).json({
+      message: "Failed to update barangay with cafgu",
+      error: err.message,
+    });
+  }
+});
+
+router.put("/:id/purok-chair", async (req, res) => {
+  const { id } = req.params;
+  const { purok_chair } = req.body;
+
+  if (!Array.isArray(purok_chair) || purok_chair.length === 0) {
+    return res
+      .status(400)
+      .json({ message: "Purok Chair array is required and cannot be empty." });
+  }
+
+  try {
+    const updatedBarangay = await Barangay.findByIdAndUpdate(
+      id,
+      { $addToSet: { purok_chair: { $each: purok_chair } } },
+      { new: true }
+    );
+
+    if (!updatedBarangay) {
+      return res.status(404).json({ message: "Barangay not found" });
+    }
+
+    res.json({
+      message: "Purok Chair added to barangay successfully",
+      barangay: updatedBarangay,
+    });
+  } catch (err) {
+    console.error("Error updating barangay with Purok Chair:", err);
+    res.status(500).json({
+      message: "Failed to update barangay with Purok Chair",
+      error: err.message,
+    });
+  }
+});
+
+router.put("/:id/tagapamayapa", async (req, res) => {
+  const { id } = req.params;
+  const { tagapamayapa } = req.body;
+
+  if (!Array.isArray(tagapamayapa) || tagapamayapa.length === 0) {
+    return res
+      .status(400)
+      .json({ message: "Purok Chair array is required and cannot be empty." });
+  }
+
+  try {
+    const updatedBarangay = await Barangay.findByIdAndUpdate(
+      id,
+      { $addToSet: { tagapamayapa: { $each: tagapamayapa } } },
+      { new: true }
+    );
+
+    if (!updatedBarangay) {
+      return res.status(404).json({ message: "Barangay not found" });
+    }
+
+    res.json({
+      message: "Purok Chair added to barangay successfully",
+      barangay: updatedBarangay,
+    });
+  } catch (err) {
+    console.error("Error updating barangay with Purok Chair:", err);
+    res.status(500).json({
+      message: "Failed to update barangay with Purok Chair",
+      error: err.message,
+    });
+  }
+});
+
 export default router;
