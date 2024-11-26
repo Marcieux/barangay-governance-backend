aabf040ed5d62ba78d2dcb056c3f0c08df582e95
Marc Jerome Doria <marcj.doria@gmail.com>
2024-11-26 16:08:03 +0800

    added a municipality field in the barangay model

--- models/barangays-model.js
+++ models/barangays-model.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const barangaySchema = mongoose.Schema({
   barangay_name: { type: String, required: true },
+  municipality: { type: String, required: true },
   king_id: { type: mongoose.Schema.Types.ObjectId, ref: "People", default: null },
   king_name: { type: String, default: null },
   prince: [{ type: mongoose.Schema.Types.ObjectId, ref: "Prince", required: true }],
