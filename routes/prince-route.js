2863118a1aae6871fea5ac1ce702c3e54f853c1e
Marc Jerome Doria <marcj.doria@gmail.com>
2024-12-06 13:36:36 +0800

    renamed params for better readability
    -removed unused put request

--- routes/prince-route.js
+++ routes/prince-route.js
@@ -15,18 +15,18 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
+router.get("/:prince_id", async (req, res) => {
+  const { prince_id } = req.params;
 
   try {
-    const prince = await Prince.findById(id);
+    const prince = await Prince.findOne({ prince_id });
 
     if (!prince) {
       return res.status(404).json({ message: "Prince not found" });
     }
     res.json(prince);
   } catch (err) {
-    console.error("Error retrieving prince by ID:", err);
+    console.error("Error retrieving prince by prince_id:", err);
     res.status(500).json({ message: "Failed to retrieve prince", error: err.message });
   }
 });
@@ -50,40 +50,6 @@ router.post("/", async (req, res) => {
     res
       .status(500)
       .json({ message: "Failed to create prince", error: err.message });
-  }
-});
-
-router.put("/:id/general", async (req, res) => {
-  const { id } = req.params;
-  const { general } = req.body;
-
-  if (!Array.isArray(general) || general.length === 0) {
-    return res
-      .status(400)
-      .json({ message: "Generals array is required and cannot be empty." });
-  }
-
-  try {
-    const updatedPrince = await Prince.findByIdAndUpdate(
-      id,
-      { $addToSet: { general: { $each: general } } },
-      { new: true }
-    );
-
-    if (!updatedPrince) {
-      return res.status(404).json({ message: "Barangay not found" });
-    }
-
-    res.json({
-      message: "Generals added to barangay successfully",
-      prince: updatedPrince,
-    });
-  } catch (err) {
-    console.error("Error updating barangay with generals:", err);
-    res.status(500).json({
-      message: "Failed to update barangay with generals",
-      error: err.message,
-    });
   }
 });
 
